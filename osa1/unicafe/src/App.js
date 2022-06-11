63a9ae3215cc79eec0ffd5e1d87dc464941534ee
Petri Vainio <petri.vainio97@gmail.com>
2022-06-11 15:50:38 +0300

    osa 1.8

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -6,6 +6,21 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
+const Statistics = ({good, neutral, bad}) => {
+  return(
+    <div>
+      <h1>statistics</h1>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {good + neutral + bad}</div>
+      <div>average {(good - bad)/(good + neutral + bad)}</div>
+      <div>positive {good/ (good + neutral + bad)*100} %</div>
+    </div>
+  
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -30,15 +45,7 @@ const App = () => {
       <Button handleClick={handleGoodClick} text='good'/> 
       <Button handleClick={handleNeutralClick} text='neutral'/> 
       <Button handleClick={handleBadClick} text='bad'/> 
-
-      <h1>statistics</h1>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {good + neutral + bad}</div>
-      <div>average {(good - bad)/(good + neutral + bad)}</div>
-      <div>positive {good/ (good + neutral + bad)*100} %</div>
-
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
