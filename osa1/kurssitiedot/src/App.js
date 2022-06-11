f51982eaf2ca0509dbc8f91451f604789b781793
Petri Vainio <petri.vainio97@gmail.com>
2022-06-11 12:39:50 +0300

    osa 1.2

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -9,9 +9,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <Part part={props.part1} exercises={props.exercises1}/>
+      <Part part={props.part2} exercises={props.exercises2}/>
+      <Part part={props.part3} exercises={props.exercises3}/>
     </div>
   )
 }
@@ -24,6 +24,13 @@ const Total = (props) => {
   )
 }
 
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.part} {props.exercises}</p>
+    </div>
+  )
+}
 
 const App = (props) => {
   const course = 'Half Stack application development'
@@ -39,7 +46,6 @@ const App = (props) => {
       <Header course={course} />
       <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-
     </div>
   )
 }
