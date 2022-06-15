3a2fe4b0cde217f66e1bdfc5ebed373e8267d1cd
Petri Vainio <petri.vainio97@gmail.com>
2022-06-15 17:59:06 +0300

    osa 2.6

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+
+const Person = ({name}) => {
+  console.log(name)
+  return (
+    <div>
+      <p>{name}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+    }
+  
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNoteChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person =>
+        <Person key={person.name} name={person.name}/>)}
+    </div>
+  )
+
+}
+
+export default App
