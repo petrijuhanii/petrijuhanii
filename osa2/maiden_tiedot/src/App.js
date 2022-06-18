7903c51279f6de78942f4587cac82799e820f7f8
Petri Vainio <petri.vainio97@gmail.com>
2022-06-18 15:00:56 +0300

    osa 2.12

--- osa2/maiden_tiedot/src/App.js
+++ osa2/maiden_tiedot/src/App.js
@@ -1,11 +1,29 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Country = ({country}) => {
+const Country = ({country, showDetails}) => {
+  console.log(Object.values(country.languages))
+  if(showDetails){
+    return (
+      <div>
+        <h1>{country.name.common}</h1>
+        <div>capital {country.capital}</div>
+        <div>area {country.area}</div>
+        <h2>languages: </h2>
+        <ul>
+        {Object.values(country.languages).map(lang=>(
+          <li key={lang}>{lang}</li>))}
+        </ul>
+        <img src={country.flags.svg} alt={country.name.common} width="150px" />
+
+      </div>
+    )
+  }
   return (
     <div>
       {country.name.common}
     </div>
+    
   )
 }
 
@@ -21,6 +39,7 @@ const App = () => {
   const [countries, setCountries] = useState([]) 
   const [newFilter, setNewFilter] = useState('')
   const [showAll, setShowAll] = useState(false)
+  const [showDetails, setShowDetails] = useState(false)
 
   useEffect(() => {
     console.log('effect')
@@ -36,11 +55,17 @@ const App = () => {
   const handleFilterChange = (event) => {
     console.log(event.target.value)
     setNewFilter(event.target.value.toLowerCase())
-    if(countries.filter(country => country.name.common.toLowerCase().includes(event.target.value.toLowerCase())).length<=10){
+    let countriesLen = countries.filter(country => country.name.common.toLowerCase().includes(event.target.value.toLowerCase())).length
+    if(countriesLen<=10){
       setShowAll(true)
+      setShowDetails(false)
+      if(countriesLen===1){
+        setShowDetails(true)
+      }
     }
     else{
       setShowAll(false)
+      setShowDetails(false)
     }
   }
   
@@ -48,13 +73,13 @@ const App = () => {
   ? countries.filter(country => country.name.common.toLowerCase().includes(newFilter))
   : []
     
-
+console.log(countriesToShow)
   return (
     <div>
       <Filter value={newFilter} onChange={handleFilterChange}/>
       
       {countriesToShow.map(country =>
-        <Country key={country.name} country={country}/>)}
+        <Country key={country.population} country={country} showDetails={showDetails}/>)}
     </div>
   )
 
