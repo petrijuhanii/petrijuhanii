6b7e9076ac76ee3f396a220dc1622a8d74253112
Petri Vainio <petri.vainio97@gmail.com>
2022-06-19 16:43:34 +0300

    osat 3.5 ja 3.6

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   {
     id: 1,
@@ -46,6 +48,43 @@ let persons = [
     persons = persons.filter(person => person.id !== id)
   
     response.status(204).end()
+  })
+
+  const generateId = () => {
+    const id = Math.random(9999999)
+    console.log(id)
+    return id
+  }
+  
+  app.post('/api/persons', (request, response) => {
+    const body = request.body
+    console.log(request.body)
+
+    if (!body.name) {
+      return response.status(400).json({ 
+        error: 'name missing' 
+      })
+    }
+    if (!body.number) {
+        return response.status(400).json({ 
+          error: 'number missing' 
+        })
+      }
+    if(persons.some(item => item.name === body.name)){
+      return response.status(400).json({ 
+        error: 'name must be unique' 
+      })
+    }
+  
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+    console.log(person)
+    persons = persons.concat(person)
+  
+    response.json(person)
   })
 
   const PORT = 3001
