983ecfec59a6f0a429a6b101611e648978eb8a55
Petri Vainio <petri.vainio97@gmail.com>
2022-09-02 14:10:29 +0300

    tehtävät osaan 6.8 asti

---
+++ osa6/redux-anecdotes/src/App.js
@@ -0,0 +1,14 @@
+import NewAnecdote from './components/AnecdoteForm'
+import Anecdotes from './components/AnecdoteList'
+
+const App = () => {
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <Anecdotes  />
+      <NewAnecdote />
+    </div>
+  )
+}
+
+export default App
