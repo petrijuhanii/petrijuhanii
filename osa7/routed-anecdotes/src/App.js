53ed4ad37cbafd5cf82e5073d49f4eba1838beaa
Petri Vainio <petri.vainio97@gmail.com>
2022-09-14 13:34:58 +0300

    osat 7.1-6

--- osa7/routed-anecdotes/src/App.js
+++ osa7/routed-anecdotes/src/App.js
@@ -30,6 +30,7 @@ const Menu = ({ anecdotes, addNew }) => {
 const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
+    
     <ul>
       {anecdotes.map(anecdote => 
         <li key={anecdote.id} >
